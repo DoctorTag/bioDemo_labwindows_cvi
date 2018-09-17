f17b43d1e0785919f08304112971a27e4f10917c
seanfan <seanfan@qq.com>
2018-09-17 22:03:13 +0800

    Add lic header

--- serial.c
+++ serial.c
@@ -1,16 +1,37 @@
-/********************************************************************************************/
-/********************************************************************************************/
-/* File:    InstallCommCallback.c                                                           */
-/* Purpose: This example illustrates the use of the RS232 Library's InstallComCallback      */
-/*          function to respond to the LWRS_RXFLAG event. The user will be asked to enter   */
-/*          an event character. Whenever this event character is detected on the COM port,  */
-/*          a callback function will be executed.                                           */
-/* Notes:   To run this example you need to do a Loopback Test, i.e. connect pin 2 to       */
-/*          pin 3 on the serial port.                                                       */
-/*          The example uses port 1 by default. To use another port, change the COM_PORT    */
-/*          defined value.                                                                  */
-/********************************************************************************************/
-/********************************************************************************************/
+/*---------------------------------------------------------------------------
+
+BSD 3-Clause License
+
+Copyright (c) 2018, feelkit
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+---------------------------------------------------------------------------*/
 
 /********************************************************************************************/
 /* Include files                                                                            */
